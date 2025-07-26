94e0e76d3acf7e39313dce1c92bd92652411f675
choiseoji <choiseoji123@naver.com>
2025-07-27 03:39:53 +0900

    feat: 스웨거 설명 추가

--- src/main/java/com/backend/domain/place/dto/request/PlaceRecommendRequestDto.java
+++ src/main/java/com/backend/domain/place/dto/request/PlaceRecommendRequestDto.java
@@ -1,18 +1,19 @@
 package com.backend.domain.place.dto.request;
-
 import com.backend.domain.place.enums.PlaceType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "장소 추천받는 DTO")
 public record PlaceRecommendRequestDto(
 
-        @Schema(description = "경도")
+        @Schema(description = "현재 경도")
         String mapX,
-        @Schema(description = "위도")
+        @Schema(description = "현재 위도")
         String mapY,
         @Schema(description = "장소 유형 : TOURIST(관광지), CULTURAL(문화시설), SHOPPING(쇼핑), RESTAURANT(레스토랑)")
         PlaceType placeType,
-        @Schema(description = "반경 ")
+        @Schema(description = "반경 ~m")
         String radius
 
 ) {
 }
+
